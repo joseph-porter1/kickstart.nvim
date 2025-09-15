1d1c759d9394d3f97b700db31067984888970797
Joseph Porter <joseph@porterfamily.co>
2025-09-15 09:58:59 -0400

    add nvim-tree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,29 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('nvim-tree').setup {}
+      vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Toggle Nvim Tree' })
+    end,
+  },
+
+  --   {
+  --     'akinsho/toggleterm.nvim',
+  --     version = '*',
+  --     config = function()
+  --       require('toggleterm').setup {}
+  --       vim.keymap.set('n', '<leader>t', ':ToggleTerm<CR>', { desc = 'Toggle Terminal' })
+  --     end,
+  --   },
+  --
+  --   {
+  --     'nvim-lualine/lualine.nvim',
+  --     dependencies = { 'nvim-tree/nvim-web-devicons' },
+  --     config = function()
+  --       require('lualine').setup {
+  --         options = { theme = 'auto' },
+  --       }
+  --     end,
+  --   },
+}
